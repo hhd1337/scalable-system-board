a2d05554fddec268faa459f4f5a255cf97a8316d
goeka <goeka1337@gmail.com>
2026-08-04 11:13:41 +0900

    test(article): 게시글 CRUD API 테스트 추가

---
+++ service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
@@ -0,0 +1,71 @@
+package kuke.board.article.api;
+
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my content", 1L, 1L
+        ));
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest() {
+        ArticleResponse response = read(342827749637906432L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get()
+                .uri("v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest() {
+        update(342827749637906432L);
+        ArticleResponse response = read(342827749637906432L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse update(Long articleId) {
+        return restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("제목1", "내용내용내용"))
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
